7329606a68862f8b301597efd509074f7b715ecf
Kauser <jaffari@gmx.de>
2020-12-11 19:47:21 +0100

    updating

---
+++ src/components/Nav.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Nav() {
+    return (
+        <div>
+            <h2>Good bye</h2>
+        </div>
+    )
+}
---
+++ src/components/Prices.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Prices() {
+    return (
+        <div>
+            <h1>   Prices of cars </h1>
+        </div>
+    )
+}
